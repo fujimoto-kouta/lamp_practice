b924cf4b7d15c17d2e57338a8cbdcc9f52a1360e
fujimoto kouta <89trghm_no.1_ga.ta.ro-hi.ry02@ezweb.ne.jp>
2020-03-29 05:10:17 +0900

    各SQL文を変更しました

--- www/model/user.php
+++ www/model/user.php
@@ -14,11 +14,11 @@ function get_user($db, $user_id){
     FROM
       users
     WHERE
-      user_id = {$user_id}
+      user_id = ?
     LIMIT 1
   ";
 
-  return fetch_query($db, $sql);
+  return fetch_query($db, $sql, [$user_id]);
 }
 
 function get_user_by_name($db, $name){
@@ -31,11 +31,11 @@ function get_user_by_name($db, $name){
     FROM
       users
     WHERE
-      name = '{$name}'
+      name = ?
     LIMIT 1
   ";
 
-  return fetch_query($db, $sql);
+  return fetch_query($db, $sql, [$name]);
 }
 
 function login_as($db, $name, $password){
@@ -116,9 +116,9 @@ function insert_user($db, $name, $password){
   $sql = "
     INSERT INTO
       users(name, password)
-    VALUES ('{$name}', '{$password}');
+    VALUES (?, ?);
   ";
 //インサート実行をリターン
-  return execute_query($db, $sql);
+  return execute_query($db, $sql, [$name, $password]);
 }
 
